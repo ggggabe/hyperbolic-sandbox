15eae8e8f3b70d2356d1ea895109f78212729ea9
Gabe Marquez <gabemarquez@protonmail.com>
2020-02-23 16:41:13 -0500

    Add another react container for nice loading of background image

---
+++ src/Content.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react'
+import ProgressiveImage from 'react-progressive-image'
+import Blur from 'react-blur'
+
+export default function () {
+  const config = {
+    src: [process.env.PUBLIC_URL, 'sky.jpeg'].join('/'),
+    placeholder: [process.env.PUBLIC_URL, 'sky-tiny.jpg'].join('/')
+  }
+
+  const [src, setSrc] = useState(config.placeholder)
+
+  const [blurRadius, setBlurRadius] = useState(20)
+  const sharpen = () => setBlurRadius(blurRadius - 1)
+
+  useEffect(() => {
+    if (blurRadius <= 0 || src === config.placeholder) return
+    const animate = setInterval(sharpen, 30)
+
+    return () => clearInterval(animate)
+  }, [blurRadius, src, config])
+
+
+  return <ProgressiveImage {...config}>
+      {newSrc => {
+        setSrc(newSrc)
+        return <Blur img={newSrc} blurRadius={blurRadius} enableStyles/>
+      }}
+  </ProgressiveImage>
+}
+
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import Content from './Content'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('webgl'))
+ReactDOM.render(<Content />, document.getElementById('container'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
